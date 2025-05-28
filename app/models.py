44395e1868594fab9e681765faab27183a6031f4
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-28 20:11:37 +0300

    correct col data

--- app/models.py
+++ app/models.py
@@ -1,21 +1,23 @@
 from app.extensions import db
 
 class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    students = db.relationship("student", back_populates="course")
+    __tablename__ = 'courses'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    students = db.relationship("Student", back_populates="course")
 
     def __repr__(self):
         return f"<Course {self.name}>"
-    
+
 class Student(db.Model):
+    __tablename__ = 'students'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100),unique=True, nullable=False)
-    course_id = db.Column(db.ForeignKey("course_id"))
-    
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"))
+
     course = db.relationship("Course", back_populates="students")
-    
 
     def __repr__(self):
         return f"<Student {self.name}>"
